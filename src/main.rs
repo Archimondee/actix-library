5d802f1c8d07d8f3d80b23efd55fd22ad5f17801
Gilang <gilangdev97@gmail.com>
2024-11-15 11:15:51 +0700

    add health route handlers

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,20 @@
 mod db;
+mod handlers;
 mod models;
+mod queries;
 mod schema;
 
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use db::establish_connection;
+
+use handlers::health_handlers::configure_health_handlers;
 use log::info;
-//use serde::Serialize;
 use simplelog::{Config, SimpleLogger};
 
 fn init_logger() {
     SimpleLogger::init(log::LevelFilter::Debug, Config::default())
         .expect("Failed to initialize logger");
 }
-
-// #[derive(Serialize)]
-// struct ErrorResponse {
-//     error: String,
-// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -25,8 +23,13 @@ async fn main() -> std::io::Result<()> {
     info!("Application is starting up...");
     let pool = establish_connection();
 
-    HttpServer::new(move || App::new().app_data(web::Data::new(pool.clone())))
-        .bind("127.0.0.1:5100")?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(pool.clone()))
+            .wrap(Logger::new("%a %T \"%r\" %s %b %Dms"))
+            .service(web::scope("/v1").configure(configure_health_handlers))
+    })
+    .bind("127.0.0.1:5100")?
+    .run()
+    .await
 }
